d85715e3bdf6f742604b5ccc2dc2e023cc3d28cc
opilane <ang.tulusa@gmail.com>
2023-09-25 12:56:40 +0300

    cat

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <script>
-	import successkid from 'images/successkid.jpg';
+	import cat from 'images/cat.jpg';
 </script>
 
 <style>
@@ -43,7 +43,7 @@
 <h1>Great success!</h1>
 
 <figure>
-	<img alt="Success Kid" src="{successkid}">
+	<img alt="CAT" src="{cat}">
 	<figcaption>Have fun with Sapper!</figcaption>
 </figure>
 
